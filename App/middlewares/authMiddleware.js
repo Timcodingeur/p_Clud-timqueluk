e32de6f25131971e0b8bfb9a7b3b317529cc29ba
Timothy Noah Jotterand <pw67ehi@eduvaud.ch>
2024-10-11 13:55:13 +0200

    supp de tout

--- App/middlewares/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-function isAuthenticated(req, res, next) {
-    if (req.session && req.session.isAuthenticated) {
-        return next();
-    } else {
-        res.redirect('/auth/signin');
-    }
-}
-
-module.exports = { isAuthenticated };
